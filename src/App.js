c61da637a7db7843f433ca5fcb2b2825c5c0f6b8
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-06-04 22:09:43 -0300

    Implemented done components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import TextEditor from './components/TextEditor.js';
 import TextPreview from './components/TextPreview.js';
-import './components/TextPreview.js';
 import React from 'react';
 
 class App extends React.Component {
